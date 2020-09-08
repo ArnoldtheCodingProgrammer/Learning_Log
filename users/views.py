f203d4276f476156f3c3db0f25080162182c184a
arnoldcheung <arnoldcheung1998@gmail.com>
2020-09-08 19:54:44 +1000

    Added comment.

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# Create your views here. #Hi
 def register(request):
     """Register a new user."""
     if request.method != 'POST':
