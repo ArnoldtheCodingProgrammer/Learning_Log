af833e063e9043939f27228960ac995fc1bb9df8
ArnoldtheCodingProgrammer <63386187+ArnoldtheCodingProgrammer@users.noreply.github.com>
2021-01-30 20:24:28 +1100

    Removed SECRET KEY for public

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^dazvd1s#oqxrxs3j@ah-%$su4v+6ybm)6ql%$k44pxuih_20c'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
